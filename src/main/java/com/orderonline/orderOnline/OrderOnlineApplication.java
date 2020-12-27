3592e88e52fe64ac7d13d51d152fda00d47eb0f6
Rahul bhatt <rahul.bhatt@plastiq.com>
2020-12-26 22:09:10 -0800

    boiler plate code

---
+++ src/main/java/com/orderonline/orderOnline/OrderOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.orderonline.orderOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderonline/orderOnline/OrderOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderonline.orderOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
